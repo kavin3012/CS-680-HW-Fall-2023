ee5d90d33a2170035311f688349f5cdc578b0272
kavin3012 <113407225+kavin3012@users.noreply.github.com>
2023-11-06 16:13:39 -0500

    Update ViewingState.java

--- hw04/src/edu/umb/cs680/hw04/ViewingState.java
+++ hw04/src/edu/umb/cs680/hw04/ViewingState.java
@@ -10,12 +10,12 @@ public class ViewingState implements DocumentState{
 
     @Override
     public void editDocument() {
-        System.out.println("You cannot edit the document while it's in Viewing state. Please make a copy if you want to edit.");
+        document.setCurrentState(new EditingState(document));
     }
 
     @Override
     public void saveDocument() {
-        System.out.println("You cannot save the document while it's being viewed. Please exit the Viewing mode.");
+        document.setCurrentState(new SavingState(document));
     }
 
     @Override
@@ -23,10 +23,5 @@ public class ViewingState implements DocumentState{
         System.out.println("You are already in the Viewing state. Enjoy reading the document.");
     }
 
-    @Override
-    public void exitDocument() {
-        System.out.println("You are exiting the Viewing state. Enjoy editing the document......");
-        document.setCurrentState(new EditingState(document));
-    }
 
 }
