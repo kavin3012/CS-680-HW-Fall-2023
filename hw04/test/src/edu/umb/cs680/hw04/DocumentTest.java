c01ea462f1217f0ee0661ebf5bb1f4b6215748ac
kavin3012 <113407225+kavin3012@users.noreply.github.com>
2023-11-06 16:12:11 -0500

    Update DocumentTest.java

--- hw04/test/src/edu/umb/cs680/hw04/DocumentTest.java
+++ hw04/test/src/edu/umb/cs680/hw04/DocumentTest.java
@@ -13,31 +13,81 @@ public class DocumentTest {
     }
 
     @Test
-    public void testEditDocument() {
+    public void testEditDocumentEdit() {
         Document document = new Document();
         document.editDocument();
         assertTrue(document.getCurrentState() instanceof EditingState);
     }
 
     @Test
-    public void testSaveDocument() {
+    public void testEditDocumentSave() {
         Document document = new Document();
         document.saveDocument();
         assertTrue(document.getCurrentState() instanceof SavingState);
     }
 
     @Test
-    public void testViewDocument() {
+    public void testEditDocumentView() {
+        Document document = new Document();
+        document.viewDocument();
+        assertTrue(document.getCurrentState() instanceof ViewingState);
+    }
+
+
+    @Test
+    public void testSaveDocumentEdit() {
+        Document document = new Document();
+        document.saveDocument();
+        document.editDocument();
+        assertTrue(document.getCurrentState() instanceof EditingState);
+    }
+
+    @Test
+    public void testSaveDocumentView() {
         Document document = new Document();
         document.saveDocument();
         document.viewDocument();
         assertTrue(document.getCurrentState() instanceof ViewingState);
     }
 
+
+
     @Test
-    public void testExitDocument() {
+    public void testSaveDocumentSave() {
         Document document = new Document();
-        document.exitDocument();
+        document.saveDocument();
+        document.saveDocument();
+        assertTrue(document.getCurrentState() instanceof SavingState);
+    }
+
+
+
+
+
+    @Test
+    public void testViewDocumentEdit() {
+        Document document = new Document();
+        document.viewDocument();
+        document.editDocument();
         assertTrue(document.getCurrentState() instanceof EditingState);
     }
+
+    @Test
+    public void testViewDocumentSave() {
+        Document document = new Document();
+        document.viewDocument();
+        document.saveDocument();
+        assertTrue(document.getCurrentState() instanceof SavingState);
+    }
+
+    @Test
+    public void testViewDocumentView() {
+        Document document = new Document();
+        document.viewDocument();
+        document.viewDocument();
+        assertTrue(document.getCurrentState() instanceof ViewingState);
+    }
+
+
+
 }
